ca5939f426bd0152c5a601cb4ec87398263420e9
Maksim Alipau <maxanotest@gmail.com>
2025-01-31 11:12:34 +0300

    feat: add a task 1 from Innodom

--- hw/task1.js
+++
@@ -1,7 +0,0 @@
-const number = +prompt('Введите число с дробной частью:');
-
-console.log(`Округление до целого: ${Math.round(number)}`);
-console.log(`Округление до целого в меньшую сторону: ${Math.floor(number)}`);
-console.log(`Округление до целого в большую сторону: ${Math.ceil(number)}`);
-console.log(`Удаление дробной части: ${Math.trunc(number)}`);
-console.log(`Форматирование до двух знаков после запятой: ${number.toFixed(2)}`);
---
+++ hw2/task1.js
@@ -0,0 +1,6 @@
+const str = 'inodm';
+console.log(str[0]); // 'i'
+console.log(str[2]); // 'o'
+console.log(str[3]); // 'd'
+
+    
